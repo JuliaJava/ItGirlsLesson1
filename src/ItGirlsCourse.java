8330e0220344dd37a0a79c0127ef7717148b5727
Julia_Sheveleva <julia_sheveleva@senla.eu>
2024-10-23 17:46:40 +0300

    Lesson 11. Threads

--- src/ItGirlsCourse.java
+++ src/ItGirlsCourse.java
@@ -1,80 +1,76 @@
-
-import itGirlsSchool.lesson10.ExceptionExample;
-import itGirlsSchool.models.Admin;
 import itGirlsSchool.models.User;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 public class ItGirlsCourse {
 
+    private static int countOfUsers = 0;
+
+    private static Object lock = new Object();
+
+    public synchronized static void incrementCount () {
+        countOfUsers++;
+    }
+
     public static void main(String[] args) throws Throwable {
 
-        User zeroUser = new User("Vitold", "vitold@gmail.com", "649854");
-        User secondUser = new User("Bonnie", "bonie@gmail.com", "967865");
+        User.registerNewUser("Thread:" + Thread.currentThread().getName() + ". User Main", "klsdfjsd", "a;erjWEIOJXL");
+        Thread myThread1 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    System.out.println("Waiting message....");
+                    try {
+                        lock.wait();
+                        System.out.println("Message received");
+                    } catch (InterruptedException e) {
+                        System.out.println("Exception:(");
+                        throw new RuntimeException(e);
+                    }
+                }
+                User.registerNewUser(Thread.currentThread().getName() + " User Thread1", "klsdfjsd", "a;erjWEIOJXL");
+                for (int i = 0; i < 1000; i++) {
+                    incrementCount();
+                }
 
-        Admin admin = new Admin("Admin", "admin", "admin@admin.com");
-        zeroUser.addFollower();
-        zeroUser.addFollower();
-        secondUser.addFollower();
-        secondUser.addFollower();
-        System.out.println(zeroUser);
+                System.out.println("Count of users: " + countOfUsers);
+            }
+        };
+        MyRunnable myRunnable = new MyRunnable();
+        Thread myThreadRunnable = new Thread(myRunnable);
 
-        System.out.println("Admin " + admin.getUserName() + " has password " + admin.getPassword());
+        Runnable myRunnable2 = () -> {
+            User.registerNewUser("Thread:" + Thread.currentThread().getName() + ". User Runnable", "klsdfjsd", "a;erjWEIOJXL");
+            for (int i = 0; i < 1000; i++) {
+                incrementCount();
+            }
+            System.out.println("Count of users: " + countOfUsers);
+        };
 
-        admin.setNewPassword("admin1");
-        System.out.println("Admin " + admin.getUserName() + "has password " + admin.getPassword());
+        Thread myThreadRunnable2 = new Thread(myRunnable2);
+        myThread1.start();
+        Thread.sleep(10000);
+        myThreadRunnable.start();
+        myThreadRunnable2.start();
+        myThread1.join();
+        myThreadRunnable.join();
+        myThreadRunnable2.join();
 
-        System.out.println(zeroUser.checkPassword("34234"));
-        User.printCountOfFollowersForAllUsers();
-        secondUser.logIn();
-        secondUser.sendMessage("Привет!!!");
-        secondUser.logOut();
-        admin.logIn();
-        admin.sendMessage("Тоже привет!!!!");
-        admin.logOut();
-
-        List<Integer> numbers = Arrays.asList(5, 89, 45, 22, 10, 2);
-        List<Integer> numbersFiltered = numbers.stream().
-                filter(numb -> numb % 2 == 0).
-                map(numb -> numb * 2).
-                toList();
-
-        List<Integer> numbersSorted = numbers.stream().
-                sorted(Comparator.reverseOrder()).
-                toList();
-
-        List<User> users = new ArrayList<>();
-        users.add(zeroUser);
-        users.add(secondUser);
-        users.add(new User("Obi", "obi@gmail.com", "68496546+rfg"));
-        users.add(new User("One", "one@gmail.com", "213"));
-        users.add(new User("Kenobi", "kenobi@gmail.com", "654984asdasd1"));
-
-        String vowels = "aeiou";
-        vowels.contains("каждая буква в слове");
-
-        List<User> usersWithO = users.stream().
-                filter(user -> user.getUserName().startsWith("O")).
-                toList();
-        System.out.println(usersWithO);
+        System.out.println("Count of users in the end: " + countOfUsers);
 
 
-        users.stream().
-                filter(user -> user.getPassword().length() <= 6).
-                forEach(user -> {
-                    System.out.println(user.getPassword());
-                    user.setNewPassword("DFSFEFXrfgsdz213");
-                });
 
 
-        ExceptionExample.checkedException1();
 
-        User thirdUser = User.registerNewUser("Boris", "sldfj", "5364641efwfsdf");
-        User fourUser = User.registerNewUser("Boris", "sldfj", "5364641efwfsdf");
-        User fiveUser = User.registerNewUser("Boris1", "sldfj", "231");
+
+    }
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                System.out.println("Message send");
+                lock.notify();
+            }
+        }
     }
 
 
